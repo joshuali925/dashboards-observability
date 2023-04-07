d2aa51a2b165885becea8bc77c872b5dcad19eea
Joshua Li <joshuali925@gmail.com>
2023-04-07 17:23:50 +0000

    --wip--
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    (cherry picked from commit 1d34427aa4fc71ee53f80315f893a8ceecc98f0e)

---
+++ public/services/saved_objects/saved_object_client/types.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { SavedQuery, SavedVisualization } from '../../../../common/types/explorer';
+import { SAVED_QUERY, SAVED_VISUALIZATION } from '../../../../common/constants/explorer';
+
+export interface SavedObjectsCreateResponse {
+  objectId: string;
+}
+
+export type SavedObjectsUpdateResponse = SavedObjectsCreateResponse;
+
+interface ObservabilitySavedObjectBase {
+  createdTimeMs: number;
+  lastUpdatedTimeMs: number;
+  objectId: string;
+  tenant?: string;
+}
+
+export interface ObservabilitySavedVisualization extends ObservabilitySavedObjectBase {
+  [SAVED_VISUALIZATION]: SavedVisualization;
+}
+
+export interface ObservabilitySavedQuery extends ObservabilitySavedObjectBase {
+  [SAVED_QUERY]: SavedQuery;
+}
+
+export type ObservabilitySavedObject = ObservabilitySavedVisualization | ObservabilitySavedQuery;
+
+export interface SavedObjectsGetParams {
+  objectId: string;
+}
+
+export interface SavedObjectsGetResponse {
+  startIndex?: number;
+  totalHits?: number;
+  totalHitRelation?: 'eq' | 'gte';
+  observabilityObjectList: ObservabilitySavedObject[];
+}
+
+export interface SavedObjectsDeleteParams {
+  objectId: string;
+}
+
+export interface SavedObjectsDeleteBulkParams {
+  objectIdList: string[];
+}
+
+export interface SavedObjectsDeleteResponse {
+  deleteResponseList: {
+    [objectId: string]: string; // org.opensearch.rest.RestStatus, e.g. 'OK'
+  };
+}
